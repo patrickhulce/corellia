ece6ce4ce67f22742a34911ac723463d626882ed
Patrick Hulce <patrick.hulce@gmail.com>
2024-02-15 11:02:49 -0600

    scripts(setup): add docker to linux

--- src/scripts/setup/setup-linux.sh
+++ src/scripts/setup/setup-linux.sh
@@ -28,6 +28,32 @@ sudo apt-get -y install cuda-toolkit-12-3
 cd "${SCRIPT_DIR}"
 nvcc --version
 
+## START DOCKER SETUP ##
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+sudo usermod -aG docker ${USER}
+newgrp docker
+
+docker run hello-world
+docker run --rm --gpus all nvidia/cuda:12.3.1-devel-ubuntu22.04 nvidia-smi
+## END DOCKER SETUP ##
+
 # From https://docs.conda.io/projects/conda/en/latest/user-guide/install/linux.html
 mkdir -p ~/code/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/code/miniconda3/miniconda.sh
