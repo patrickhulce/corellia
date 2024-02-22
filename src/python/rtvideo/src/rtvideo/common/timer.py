52f5334f9dbd4043dc11d608ad3bef09e8801c14
Patrick Hulce <patrick.hulce@gmail.com>
2024-02-21 21:37:31 -0600

    python(rtvideo): improved timer

--- src/python/rtvideo/src/rtvideo/common/timer.py
+++ src/python/rtvideo/src/rtvideo/common/timer.py
@@ -1,22 +1,30 @@
 import time
+from collections import deque
 
+class TimerSpan:
+    def __init__(self, name):
+        self.name = name
+
+    def start(self):
+        self.start = time.time()
+
+    def end(self):
+        self.end = time.time()
+        self.duration = self.end - self.start
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.end()
 
 class Timer:
     def __init__(self):
-        self.spans = []
+        self.spans = deque(maxlen=10000)
 
     def span(self, name):
-        class TimerSpan:
-            def __enter__(self):
-                self.name = name
-                self.start = time.time()
-                return self
-
-            def __exit__(self, type, value, traceback):
-                self.end = time.time()
-                self.duration = self.end - self.start
-
-        span = TimerSpan()
+        span = TimerSpan(name)
         self.spans.append(span)
         return span
 
@@ -27,7 +35,7 @@ class Timer:
                 spans_by_name[span.name] = []
             spans_by_name[span.name].append(span)
 
-        summary = ''
+        summary_items = []
         for name, spans in spans_by_name.items():
             sorted_durations = sorted(span.duration * 1000 for span in spans)
 
@@ -36,6 +44,7 @@ class Timer:
             p95 = sorted_durations[int(len(spans) * 0.95)]
             p99 = sorted_durations[int(len(spans) * 0.99)]
 
-            summary = f"{summary}{name}: p50={p50:.2f}ms, p95={p95:.2f}ms, p99={p99:.2f}ms\n"
+            summary = f"{name}:\n\tp50={p50:.2f}ms\tp95={p95:.2f}ms\tp99={p99:.2f}ms"
+            summary_items.append((summary, p50))
 
-        return summary
+        return "\n".join(summary for summary, _ in sorted(summary_items, key=lambda x: x[1], reverse=True))
