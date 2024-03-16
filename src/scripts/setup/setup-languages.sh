07c0699be9cc7a718f4815a359be69977d9ed7eb
Patrick Hulce <patrick.hulce@gmail.com>
2024-03-15 22:56:06 -0500

    scripts(setup): add nbconvert dep

--- src/scripts/setup/setup-languages.sh
+++ src/scripts/setup/setup-languages.sh
@@ -5,4 +5,5 @@ npm install -g diff-so-fancy pkgfiles siegem yarn pnpm source-map-explorer @patr
 nvm install v18 --reinstall-packages-from=v20
 
 pipx install jupyter-core
+pipx install nbconvert
 git config --global filter.strip-notebook-output.clean 'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --to=notebook --stdin --stdout --log-level=ERROR'
