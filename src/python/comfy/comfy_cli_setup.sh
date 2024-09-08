22d0cd9e7c5e5d42832ec3da57f56e5eddd66e9d
Patrick Hulce <patrick.hulce@gmail.com>
2024-09-08 13:53:58 -0500

    python(comfy): add standard Comfy setup

---
+++ src/python/comfy/comfy_cli_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+comfy node install ComfyUI-PhotoMaker-Plus
+comfy node install ComfyUI_IPAdapter_plus
+comfy node install ComfyUI-post-processing-nodes
+comfy node install ComfyUI-DepthAnythingV2
+comfy node install ComfyUI-Custom-Scripts
+comfy node install ComfyUI-Advanced-ControlNet
+comfy node install ComfyUI_Custom_Nodes_AlekPet
+comfy node install ComfyUI-KJNodes
+comfy node install ComfyUI_essentials
+comfy node install ComfyUI_UltimateSDUpscale
+comfy node install ComfyUI-LivePortraitKJ
+comfy node install comfyui_controlnet_aux
+comfy node install ComfyUI-VideoHelperSuite
+comfy node install ComfyUI-AnimateDiff-Evolved
+comfy node install Comfyui-StableSR
