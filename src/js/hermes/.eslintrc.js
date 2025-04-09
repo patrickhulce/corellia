6a580c6d3b5a21894d375b7cd979ecbd8e2bb55f
Patrick Hulce <patrick.hulce@gmail.com>
2025-04-09 15:35:08 -0500

    js(hermes): add prettier

---
+++ src/js/hermes/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'prettier'],
+  rules: {
+    // Add any custom rules here
+  },
+}
---
+++ src/js/hermes/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  bracketSpacing: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
